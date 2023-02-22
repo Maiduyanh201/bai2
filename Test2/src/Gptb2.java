6c0837202b43d1ff0860a2bef814482a06a79df2
Maiduyanh201 <125906103+Maiduyanh201@users.noreply.github.com>
2023-02-22 17:09:58 +0700

    Add files via upload

---
+++ Test2/src/Gptb2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Gptb2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap a ");
+        float a = scanner.nextFloat();
+        System.out.println("nhap b ");
+        float b = scanner.nextFloat();
+        System.out.println("nhap c ");
+        float c = scanner.nextFloat();
+        float delta = (b*b) - (4*a*c);
+
+        double z = Math.sqrt(delta);
+        if (delta < 0){
+            System.out.println("phuong tring vo nghiem ");
+        }else if (delta > 0){
+            double x1 = (-b + z)/(2 * a);
+            double x2 = (-b - z)/(2 * a);
+            System.out.println("phuong trinh co 2 nghiem phan biet x1 =" + x1 + "x2 =" + x2 );
+        }else {
+            System.out.println("phuong trinh co nghiem kep = " + (-b) / (2 * a));
+        }
+
+    }
+}
