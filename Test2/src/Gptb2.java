5a348256f8d32fedf6c47ad18dee330687a042ed
Maiduyanh201 <125906103+Maiduyanh201@users.noreply.github.com>
2023-02-22 17:17:32 +0700

    Add files via upload

--- Test2/src/Gptb2.java
+++ Test2/src/Gptb2.java
@@ -12,12 +12,21 @@ public class Gptb2 {
         float delta = (b*b) - (4*a*c);
 
         double z = Math.sqrt(delta);
+        if (a == 0){
+            if (b == 0){
+                System.out.println("phuong trinh vo nghiem ");
+            }else {
+                System.out.println("phuong trinh co 1 nghiem = " + (-c / b));
+            }
+            return;
+        }
+
         if (delta < 0){
             System.out.println("phuong tring vo nghiem ");
         }else if (delta > 0){
             double x1 = (-b + z)/(2 * a);
             double x2 = (-b - z)/(2 * a);
-            System.out.println("phuong trinh co 2 nghiem phan biet x1 =" + x1 + "x2 =" + x2 );
+            System.out.println("phuong trinh co 2 nghiem phan biet x1 =" + x1 + " x2 =" + x2 );
         }else {
             System.out.println("phuong trinh co nghiem kep = " + (-b) / (2 * a));
         }
